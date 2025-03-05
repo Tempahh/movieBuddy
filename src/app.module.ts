30269e2999312366c4a165cac7a5c946b0b695ab
Prosper <plance991@gmail.com>
2025-03-05 08:21:22 +0100

    [feat]: update Mongoose configuration to use ConfigService for dynamic MongoDB URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
+// Ensure you have a .env file with MONGO_URI
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User, UserSchema } from './schemas/user.schema';
 import { Movie, MovieSchema } from './schemas/movie.schema';
 import { Booking, BookingSchema } from './schemas/booking.schema';
@@ -8,7 +10,14 @@ import { Booking, BookingSchema } from './schemas/booking.schema';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB_URI!), // Ensure you have a .env file with MONGO_URI
+    MongooseModule.forRootAsync({
+      // eslint-disable-next-line @typescript-eslint/require-await
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        dbName: 'movieBuddy',
+      }),
+      inject: [ConfigService],
+    }), // Ensure you have a .env file with MONGO_URI
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Movie.name, schema: MovieSchema },
